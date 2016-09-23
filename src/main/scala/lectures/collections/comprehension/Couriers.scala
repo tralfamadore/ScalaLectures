f301dd31616e38460131ccd2ca9ece21fd6a02b9
Alex Maxakov <alexandr.maxakov@gmail.com>
2016-09-23 09:49:45 +0300

    added couriesrs homework

--- src/main/scala/lectures/collections/comprehension/Couriers.scala
+++ src/main/scala/lectures/collections/comprehension/Couriers.scala
@@ -1,7 +1,7 @@
 package lectures.collections.comprehension
 
 /**
-  *  Помогите курьерам разобраться с обслуживанием адресов
+  * Помогите курьерам разобраться с обслуживанием адресов
   *
   * Каждый день на работу выходит 'courierCount' курьеров
   * Им нужно обслужить 'addressesCount' адресов
@@ -13,8 +13,8 @@ package lectures.collections.comprehension
   * Во второй - количество курьеров, вышедших на работу.
   *
   * Ваша задача:
-  *  Изучить код и переписать его так,
-  *  что бы в нем не было ни одного цикла for, ни одной переменной или мутабильной коллекции
+  * Изучить код и переписать его так,
+  * что бы в нем не было ни одного цикла for, ни одной переменной или мутабильной коллекции
   *
   * Для этого используйте функции комбинаторы: filter, filterWith, fold, map, flatMap и т.д.
   *
@@ -24,9 +24,7 @@ case class Traffic(degree: Double)
 
 object Courier {
   def couriers(courierCount: Int): List[Courier] =
-    (for (i <- 1 to courierCount) yield {
-      Courier(i)
-    }).toList
+    Stream.range(1, courierCount + 1).flatMap(i => List(Courier(i))).toList
 }
 
 case class Courier(index: Int) {
@@ -35,9 +33,7 @@ case class Courier(index: Int) {
 
 object Address {
   def addresses(addressesCount: Int): List[Address] =
-    (for (i <- 1 to addressesCount) yield {
-      Address(s"$i$i$i")
-    }).toList
+    Stream.range(1, addressesCount + 1).flatMap(i => List(Address(s"$i$i$i"))).toList
 }
 
 case class Address(postIndex: String)
@@ -56,22 +52,18 @@ object CouriersWithComprehension extends App {
   // какие адреса были обслужены
   def serveAddresses(addresses: List[Address], couriers: List[Courier]) = {
     var accum = 0
-    for (courier <- couriers;
-         trafficDegree = traffic().degree;
-         t <- 0 to courier.canServe if trafficDegree < 5 && accum < addresses.length
-    ) yield {
-      val addr = addresses(accum)
-      accum = accum + 1
-      addr
+    couriers.flatMap { c =>
+      Stream.range(0, c.canServe).withFilter(_ => traffic().degree < 5 && accum < addresses.length).flatMap { i =>
+        accum = accum + 1
+        List(addresses(accum - 1))
+      }
     }
   }
 
-  def traffic(): Traffic = new Traffic(Math.random() * 10)
+  def traffic(): Traffic = Traffic(Math.random() * 10)
 
   def printServedAddresses(addresses: List[Address], couriers: List[Courier]) =
-    for (a <- serveAddresses(addresses, couriers)) {
-      println(a.postIndex)
-    }
+    serveAddresses(addresses, couriers).foreach(a => println(a.postIndex))
 
   printServedAddresses(addrs, cours)
 
