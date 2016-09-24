07081ddbc0ce38f87d5f5ab6061f1480e8f0205c
Alex Maxakov <alexandr.maxakov@gmail.com>
2016-09-24 21:50:27 +0300

    added bstree generator impl

--- src/main/scala/lectures/oop/BST.scala
+++ src/main/scala/lectures/oop/BST.scala
@@ -30,6 +30,15 @@ trait BST {
   def add(newValue: Int): BST
 
   def find(value: Int): Option[BST]
+
+  def size(): Int = {
+    def count(node: Option[BST]): Int = node match {
+      case None => 0
+      case Some(bst: BST) => count(bst.left) + count(bst.right) + 1
+    }
+    count(this.left) + count(this.right) + 1
+  }
+
 }
 
 case class BSTImpl(value: Int,
@@ -45,7 +54,7 @@ case class BSTImpl(value: Int,
           case Some(n: BSTImpl) => BSTImpl(node.value, Some(add(n, newValue)), node.right)
         }
       } else if (node.value < newValue) {
-        node.right match  {
+        node.right match {
           case None => BSTImpl(node.value, node.left, Some(next))
           case Some(n: BSTImpl) => BSTImpl(node.value, node.left, Some(add(n, newValue)))
         }
@@ -75,6 +84,16 @@ case class BSTImpl(value: Int,
 
 object TreeTest extends App {
 
+  def getNext: Int = (Math.random() * 1000).toInt
+
+  def generator(root: BST, size: Int): BST = {
+    def generate(n: BST): BST = size - n.size() match {
+      case 0 => n
+      case _ => generate(n.add(getNext))
+    }
+    generate(root)
+  }
+
   val sc = new java.util.Scanner(System.in)
   val maxValue = 110000
   val nodesCount = sc.nextInt()
@@ -85,15 +104,22 @@ object TreeTest extends App {
 
   // Generate huge tree
   val root: BST = BSTImpl(maxValue / 2)
-//  val tree: BST = ??? // generator goes here
+  val tree: BST = generator(root, nodesCount)
 
   // add marker items
-  val testTree = root.add(markerItem).add(markerItem2).add(markerItem3)
+  val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
 
   // check that search is correct
   require(testTree.find(markerItem).isDefined)
   require(testTree.find(markerItem).isDefined)
   require(testTree.find(markerItem).isDefined)
 
+  val root2 = BSTImpl(23)
+  val testTree2 = root2.add(46).add(42).add(15).add(21).add(13).add(53)
+  require(testTree2.size() == 7)
+
   println(testTree)
+
+  println(tree.size())
+  require(tree.size() == nodesCount)
 }
